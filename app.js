8ee2fd8775231d0f9f4ecb69297ea1b0cd94ddbd
unknown <isramm94@gmail.com>
2020-02-21 16:32:11 +0200

    2nd upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 1. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
-// Sample Output : ÇßÊÈ ÏÇáÉ ÊŞæã ÈØÈÇÚÉ ÚäÇÕÑ ãÕİæİÉ ãä ÇáÃÑŞÇã æÊÍÏíÏ åá åæ ÒæÌí Ãæ İÑÏí
+// Sample Output : Ø§ÙƒØªØ¨ Ø¯Ø§Ù„Ø© ØªÙ‚ÙˆÙ… Ø¨Ø·Ø¨Ø§Ø¹Ø© Ø¹Ù†Ø§ØµØ± Ù…ØµÙÙˆÙØ© Ù…Ù† Ø§Ù„Ø£Ø±Ù‚Ø§Ù… ÙˆØªØ­Ø¯ÙŠØ¯ Ù‡Ù„ Ù‡Ùˆ Ø²ÙˆØ¬ÙŠ Ø£Ùˆ ÙØ±Ø¯ÙŠ
 // "0 is even"
 // "1 is odd"
 // "2 is even"
@@ -8,7 +8,7 @@
 
 
 // 2. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. Go to the editor
-// Test Data : ÇßÊÈ ÏÇáÉ áÅÑÌÇÚ Ãæá ä ãä ÇáÚäÇÕÑ ãä ÇáãÕİæİÉ
+// Test Data : Ø§ÙƒØªØ¨ Ø¯Ø§Ù„Ø© Ù„Ø¥Ø±Ø¬Ø§Ø¹ Ø£ÙˆÙ„ Ù† Ù…Ù† Ø§Ù„Ø¹Ù†Ø§ØµØ± Ù…Ù† Ø§Ù„Ù…ØµÙÙˆÙØ©
 // console.log(first([7, 9, 0, -2]));
 // console.log(first([],3));
 // console.log(first([7, 9, 0, -2],3));
@@ -22,13 +22,13 @@
 // []
 
 // 3. Write a simple JavaScript program to join all elements of the following array into a string.
-// Sample array : myColor = ["Red", "Green", "White", "Black"]; ÅÑÌÇÚ äÕ æÇÍÏ ãä ãÕİæİÉ ãä ÇáäÕæÕ
+// Sample array : myColor = ["Red", "Green", "White", "Black"]; Ø¥Ø±Ø¬Ø§Ø¹ Ù†Øµ ÙˆØ§Ø­Ø¯ Ù…Ù† Ù…ØµÙÙˆÙØ© Ù…Ù† Ø§Ù„Ù†ØµÙˆØµ
 // Expected Output :
 // "Red,Green,White,Black"
 // "Red,Green,White,Black"
 // "Red+Green+White+Black"
 
-// 4. Write a JavaScript program to sort the items of an array. ÊÑÊíÈ ÇáÚäÇÕÑ ÊÕÇÚÏíğÇ
+// 4. Write a JavaScript program to sort the items of an array. ØªØ±ØªÙŠØ¨ Ø§Ù„Ø¹Ù†Ø§ØµØ± ØªØµØ§Ø¹Ø¯ÙŠÙ‹Ø§
 // Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
 // Sample Output : -4,-3,1,2,3,5,6,7,8
 
@@ -36,12 +36,12 @@
 
 
 // 6. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". Go to the editor
-// Şã ÈÊÕãíã ÏÇáÉ ÊŞæã ÈÏæÑÉ Úáì ÇáÃÚÏÇÏ ãä 1 Åáì 100 æØÈÇÚÉ ßáãÉ Buzz
-// ááÚäÇÕÑ ÇáÊí ÊŞÈá ÇáŞÓãÉ Úáì 3¡ æØÈÇÚÉ Fizz 
-// ááÚäÇÕÑ ÇáÊí ÊŞÈá ÇáŞÓãÉ Úáì 5
-// ÚÏÇ Ğáß ÊØÈÚ ÇáÚÏÏ äİÓå
+// Ù‚Ù… Ø¨ØªØµÙ…ÙŠÙ… Ø¯Ø§Ù„Ø© ØªÙ‚ÙˆÙ… Ø¨Ø¯ÙˆØ±Ø© Ø¹Ù„Ù‰ Ø§Ù„Ø£Ø¹Ø¯Ø§Ø¯ Ù…Ù† 1 Ø¥Ù„Ù‰ 100 ÙˆØ·Ø¨Ø§Ø¹Ø© ÙƒÙ„Ù…Ø© Buzz
+// Ù„Ù„Ø¹Ù†Ø§ØµØ± Ø§Ù„ØªÙŠ ØªÙ‚Ø¨Ù„ Ø§Ù„Ù‚Ø³Ù…Ø© Ø¹Ù„Ù‰ 3ØŒ ÙˆØ·Ø¨Ø§Ø¹Ø© Fizz 
+// Ù„Ù„Ø¹Ù†Ø§ØµØ± Ø§Ù„ØªÙŠ ØªÙ‚Ø¨Ù„ Ø§Ù„Ù‚Ø³Ù…Ø© Ø¹Ù„Ù‰ 5
+// Ø¹Ø¯Ø§ Ø°Ù„Ùƒ ØªØ·Ø¨Ø¹ Ø§Ù„Ø¹Ø¯Ø¯ Ù†ÙØ³Ù‡
 
-// 7. Write a JavaScript program to construct the following pattern, using a nested for loop. Şã ÈØÈÇÚÉ ÇáÔßá ÇáÊÇáí ãä ÇáäÌæã
+// 7. Write a JavaScript program to construct the following pattern, using a nested for loop. Ù‚Ù… Ø¨Ø·Ø¨Ø§Ø¹Ø© Ø§Ù„Ø´ÙƒÙ„ Ø§Ù„ØªØ§Ù„ÙŠ Ù…Ù† Ø§Ù„Ù†Ø¬ÙˆÙ…
 
 // *  
 // * *  
@@ -53,7 +53,7 @@
 // color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
 // o = ["th","st","nd","rd"]
 // Write a JavaScript program to display the colors in the following way :
-// ÇßÊÈ ÈÑäÇãÌ íŞæã ÈØÈÇÚÉ ÚäÇÕÑ ÇáãÕİæİÉ ÈÇáÔßá ÇáÊÇáí
+// Ø§ÙƒØªØ¨ Ø¨Ø±Ù†Ø§Ù…Ø¬ ÙŠÙ‚ÙˆÙ… Ø¨Ø·Ø¨Ø§Ø¹Ø© Ø¹Ù†Ø§ØµØ± Ø§Ù„Ù…ØµÙÙˆÙØ© Ø¨Ø§Ù„Ø´ÙƒÙ„ Ø§Ù„ØªØ§Ù„ÙŠ
 // "1st choice is Blue ."
 // "2nd choice is Green."
 // "3rd choice is Red."
@@ -61,16 +61,16 @@
 // Note : Use ordinal numbers to tell their position.
 
 // 9. Write a JavaScript function to get a random item from an array. 
-// Şã ÈØÈÇÚÉ ÈÑäÇãÌ áØÈÇÚÉ ÚäÕÑ ÚÔæÇÆí ãä ãÕİæİÉ ãÇ
+// Ù‚Ù… Ø¨Ø·Ø¨Ø§Ø¹Ø© Ø¨Ø±Ù†Ø§Ù…Ø¬ Ù„Ø·Ø¨Ø§Ø¹Ø© Ø¹Ù†ØµØ± Ø¹Ø´ÙˆØ§Ø¦ÙŠ Ù…Ù† Ù…ØµÙÙˆÙØ© Ù…Ø§
 
 // 10. Write a JavaScript function to filter false, null, 0 and blank values from an array. Go to the editor
-// Şã ÈÈäÇÁ ÏÇáÉ ÊŞæã ÈÅÑÌÇÚ ÇáãÕİæİÉ ÈÏæä ÇáŞíã false, null, 0, ""
+// Ù‚Ù… Ø¨Ø¨Ù†Ø§Ø¡ Ø¯Ø§Ù„Ø© ØªÙ‚ÙˆÙ… Ø¨Ø¥Ø±Ø¬Ø§Ø¹ Ø§Ù„Ù…ØµÙÙˆÙØ© Ø¨Ø¯ÙˆÙ† Ø§Ù„Ù‚ÙŠÙ… false, null, 0, ""
 // Test Data :
 // console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
 // [58, "abcd", true]
 
 // 11. Write a JavaScript function to generate an array between two integers of 1 step length.
-//Şã ÈÈäÇÁ ÏÇáÉ ÊŞæã ÈØÈÇÚÉ ÇáÃÚÏÇÏ Èíä ÇáÑŞãíä 
+//Ù‚Ù… Ø¨Ø¨Ù†Ø§Ø¡ Ø¯Ø§Ù„Ø© ØªÙ‚ÙˆÙ… Ø¨Ø·Ø¨Ø§Ø¹Ø© Ø§Ù„Ø£Ø¹Ø¯Ø§Ø¯ Ø¨ÙŠÙ† Ø§Ù„Ø±Ù‚Ù…ÙŠÙ† 
 // Test Data :
 // console.log(rangeBetwee(4, 7));
 // [4, 5, 6, 7]
@@ -79,4 +79,4 @@
 
 
 // 12. Write a JavaScript program which compute, the average marks
-// Şã ÈßÊÇÈÉ ÏÇáÉ ÊÍÓÈ ÇáãÊæÓØ ÇáÚÏÏí áãÌãæÚÉ ãä ÇáÃÚÏÇÏ İí ãÕİæİÉ
+// Ù‚Ù… Ø¨ÙƒØªØ§Ø¨Ø© Ø¯Ø§Ù„Ø© ØªØ­Ø³Ø¨ Ø§Ù„Ù…ØªÙˆØ³Ø· Ø§Ù„Ø¹Ø¯Ø¯ÙŠ Ù„Ù…Ø¬Ù…ÙˆØ¹Ø© Ù…Ù† Ø§Ù„Ø£Ø¹Ø¯Ø§Ø¯ ÙÙŠ Ù…ØµÙÙˆÙØ©
