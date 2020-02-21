5e55b863586579e1ad253849a83b399060c1f362
unknown <isramm94@gmail.com>
2020-02-21 16:27:42 +0200

    initial upload

---
+++ app.js
@@ -0,0 +1,82 @@
+// 1. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+// Sample Output : ÇßÊÈ ÏÇáÉ ÊŞæã ÈØÈÇÚÉ ÚäÇÕÑ ãÕİæİÉ ãä ÇáÃÑŞÇã æÊÍÏíÏ åá åæ ÒæÌí Ãæ İÑÏí
+// "0 is even"
+// "1 is odd"
+// "2 is even"
+// ----------
+// ----------
+
+
+// 2. Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array. Go to the editor
+// Test Data : ÇßÊÈ ÏÇáÉ áÅÑÌÇÚ Ãæá ä ãä ÇáÚäÇÕÑ ãä ÇáãÕİæİÉ
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+// 3. Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"]; ÅÑÌÇÚ äÕ æÇÍÏ ãä ãÕİæİÉ ãä ÇáäÕæÕ
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
+
+// 4. Write a JavaScript program to sort the items of an array. ÊÑÊíÈ ÇáÚäÇÕÑ ÊÕÇÚÏíğÇ
+// Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+// Sample Output : -4,-3,1,2,3,5,6,7,8
+
+// 5. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'. Go to the editor
+
+
+// 6. Write a JavaScript program which iterates the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". Go to the editor
+// Şã ÈÊÕãíã ÏÇáÉ ÊŞæã ÈÏæÑÉ Úáì ÇáÃÚÏÇÏ ãä 1 Åáì 100 æØÈÇÚÉ ßáãÉ Buzz
+// ááÚäÇÕÑ ÇáÊí ÊŞÈá ÇáŞÓãÉ Úáì 3¡ æØÈÇÚÉ Fizz 
+// ááÚäÇÕÑ ÇáÊí ÊŞÈá ÇáŞÓãÉ Úáì 5
+// ÚÏÇ Ğáß ÊØÈÚ ÇáÚÏÏ äİÓå
+
+// 7. Write a JavaScript program to construct the following pattern, using a nested for loop. Şã ÈØÈÇÚÉ ÇáÔßá ÇáÊÇáí ãä ÇáäÌæã
+
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *  
+
+// 8. We have the following arrays :
+// color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+// o = ["th","st","nd","rd"]
+// Write a JavaScript program to display the colors in the following way :
+// ÇßÊÈ ÈÑäÇãÌ íŞæã ÈØÈÇÚÉ ÚäÇÕÑ ÇáãÕİæİÉ ÈÇáÔßá ÇáÊÇáí
+// "1st choice is Blue ."
+// "2nd choice is Green."
+// "3rd choice is Red."
+// - - - - - - - - - - - - -
+// Note : Use ordinal numbers to tell their position.
+
+// 9. Write a JavaScript function to get a random item from an array. 
+// Şã ÈØÈÇÚÉ ÈÑäÇãÌ áØÈÇÚÉ ÚäÕÑ ÚÔæÇÆí ãä ãÕİæİÉ ãÇ
+
+// 10. Write a JavaScript function to filter false, null, 0 and blank values from an array. Go to the editor
+// Şã ÈÈäÇÁ ÏÇáÉ ÊŞæã ÈÅÑÌÇÚ ÇáãÕİæİÉ ÈÏæä ÇáŞíã false, null, 0, ""
+// Test Data :
+// console.log(filter_array_values([58, '', 'abcd', true, null, false, 0]));
+// [58, "abcd", true]
+
+// 11. Write a JavaScript function to generate an array between two integers of 1 step length.
+//Şã ÈÈäÇÁ ÏÇáÉ ÊŞæã ÈØÈÇÚÉ ÇáÃÚÏÇÏ Èíä ÇáÑŞãíä 
+// Test Data :
+// console.log(rangeBetwee(4, 7));
+// [4, 5, 6, 7]
+// console.log(rangeBetwee(-4, 7));
+// [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
+
+
+// 12. Write a JavaScript program which compute, the average marks
+// Şã ÈßÊÇÈÉ ÏÇáÉ ÊÍÓÈ ÇáãÊæÓØ ÇáÚÏÏí áãÌãæÚÉ ãä ÇáÃÚÏÇÏ İí ãÕİæİÉ
